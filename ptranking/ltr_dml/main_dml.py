00c813c317b5bfe96d02778b3bd9840c8c4e9297
muramon <naoki.muramoto@gmail.com>
2021-02-22 16:30:04 +0900

    update main_dml

--- ptranking/ltr_dml/main_dml.py
+++ ptranking/ltr_dml/main_dml.py
@@ -1,12 +1,10 @@
-from losses.TopKPre import TopKPreLoss
-from losses.RSTopKPre import RSTopKPreLoss
 import logging
 import argparse
 logging.getLogger().setLevel(logging.INFO)
 
 parser = argparse.ArgumentParser(allow_abbrev=False)
 parser.add_argument("--pytorch_home", type=str, default="/media/dl-box/f12286fd-f13c-4fe0-a92d-9f935d6a7dbd/pretrained")
-parser.add_argument("--dataset_root", type=str, default="/media/dl-box/f12286fd-f13c-4fe0-a92d-9f935d6a7dbd/CVPR")
+parser.add_argument("--dataset_root", type=str, default="/media/dl-box/f12286fd-f13c-4fe0-a92d-9f935d6a7dbd/CVPR/ptraniking") #
 parser.add_argument("--root_experiment_folder", type=str, default="/media/dl-box/f12286fd-f13c-4fe0-a92d-9f935d6a7dbd/Project_output/Out_img_metric/ptranking_dml")
 parser.add_argument("--global_db_path", type=str, default=None)
 parser.add_argument("--merge_argparse_when_resuming", default=False, action='store_true')
@@ -20,7 +18,8 @@ if args.bayes_opt_iters > 0:
     args.reproductions = [int(x) for x in args.reproductions.split(",")]
     runner = BayesOptRunner
 else:
-    from powerful_benchmarker.runners.single_experiment_runner import SingleExperimentRunner
+    # from powerful_benchmarker.runners.single_experiment_runner import SingleExperimentRunner
+    from eval.single_experiment_runner import SingleExperimentRunner
     runner = SingleExperimentRunner
     del args.bayes_opt_iters
     del args.reproductions
@@ -30,5 +29,7 @@ if __name__ == '__main__':
     r = runner(**(args.__dict__))
     # register loss functions
     # r.register("loss", TopKPreLoss)
-    r.register("loss", RSTopKPreLoss)
-    r.run()
+    # r.register("loss", RSTopKPreLoss)
+    models_to_run = ["TopKPre", "RSTopKPre"]
+    for model_id in models_to_run:
+        r.run(model_id=model_id)
